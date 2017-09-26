7ef96576d9571c970d7cb45bf212820a66a55d03
Christopher Campbell <christopher.campbell@outlook.com>
2017-09-26 01:05:58 +0000

    added update action and function that redirects user back to root pg

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,6 +22,13 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
   
+  def update
+    @place = Place.find(params[:id])
+    
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+  
   
   private 
   
