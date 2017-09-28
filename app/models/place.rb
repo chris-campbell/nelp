713de4ff9aae7c6dfce755868389ec1bd5febffc
Christopher Campbell <christopher.campbell@outlook.com>
2017-09-28 22:21:23 +0000

    added associations for comments to Place, User & Comments models

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,13 @@
 class Place < ApplicationRecord
     belongs_to :user
+    has_many :comments
+    
+    # Google maps API
     geocoded_by :address
     after_validation :geocode
+    
+    # Core Validations for application
     validates :name, :address, :description, presence: true
     validates :name, :address, :description, length: { minimum: 3 }
+    
 end
