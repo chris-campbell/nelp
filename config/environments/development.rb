cb5d03927fa5567c191fee6729a2fba55b534c18
Christopher Campbell <christopher.campbell@outlook.com>
2017-10-10 23:07:50 +0000

    setup after_create to send auto email when notification is created

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,15 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
+   config.action_mailer.delivery_method = :test
   
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD'],
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "example.com",
+  #   authentication: "plain",
+  #   user_name: ENV['GMAIL_ADDRESS'],
+  #   password: ENV['GMAIL_PASSWORD'],
+  #   enable_starttls_auto: true
+  # }
   
   
   
