e58d403bf7f1594189d861ff37a1571efed8d3f4
Kris Camp <kris.camp@outlook.com>
2019-05-22 01:18:16 +0000

    Clean up CSS
    - Remove unwanted spaces and comments our syntax
    - Clean up places index page
    - Remove unessasary styles from past project

--- app/assets/javascripts/static_pages.js
+++
@@ -1,9 +0,0 @@
-// // $(function () {
-//   $(document).scroll(function () {
-//     var $nav = $(".navbar");
-    
-    
-//   });
-// // });
-
-// $nav.toggleClass('scrolled', console.log($(this).scrollTop()) > 720);
--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,14 +1,19 @@
 
 
 $(function () {
-
     $(document).scroll(function () {
-        
         var documentCurrentPos = $(this).scrollTop();
         const element = document.querySelector('.places');
-        const relevanceElement = element.scrollTop = 704;
-        
-        
+        const relevanceElement = element.scrollTop = 681;
+
+        // Navbar HTML
+        const navbar = "<header>" + 
+                            '<nav class="navbar navbar-light bg-light">' +
+                                '<a class="navbar-brand" href="#">Nelp</a>' + 
+                            "</nav>"+
+                       "</header>";
+                       
+        // Relevance HTML
         const relevance = "<section class='relevance'>" + "<div class='container'>" +
         "<div class='row'>" + "<div class='col-md-4'></div>" +
             "<div class='col-md-4'>" + 
@@ -20,70 +25,18 @@ $(function () {
                 "</select>" +
             "</div>" +
             "<div class='place-count'>" +
-                "<p><%= @places.length %> Places Found.</p>" +
+                // "<p><%= @places.length %> Places Found.</p>" +
             "</div>" +
             "</div>" +
             "</div>" +
         "</section>";
         
-        
-        const navbar = "<header>" + '<nav class="navbar navbar-light bg-light">' +
-                       '<a class="navbar-brand" href="#">Nelp</a>' + "</nav>"+
-                       "</header>";
-
-        
+        // Toggle logic for Nav/Relevance
         if (documentCurrentPos > relevanceElement) {
             $('.navbar').replaceWith(relevance);
         } else if (documentCurrentPos < relevanceElement) {
             $('.relevance').replaceWith(navbar);
         }
-    
+        
     });
-
-// if (documentCurrentPos > relevanceElement) {
-//     $('.navbar, .relevance').toggle()
-// }
-//     console.log(documentCurrentPos);
-// //   
-// //     const element = document.querySelector('.relevance');
-// //     const relevanceElement = element.scrollTop = 704;
-// //     var documentCurrentPos = $(this).scrollTop();
-    
-// //     if (documentCurrentPos > relevanceElement) {
-// //         console.log(documentCurrentPos);
-// //         $('.navbar, .relevance').toggle()
-// //         // $('.relevance').hide(400);
-// //         // $(".navbar").show();
-// //     } else if (documentCurrentPos < relevanceElement) {
-// //         $('.relevance').show(400);
-// //         // $(".navbar").hide();
-// //     }
-// //   });
 });
-
-
-
-
-
-
-
-
-
-
-
-// const relevance = "<section class='relevance'>" + "<div class='container'>" +
-//         "<div class='row'>" + "<div class='col-md-4'></div>" +
-//             "<div class='col-md-4'>" + 
-//                 '<select class="form-control" id="sel1">' +
-//                     "<option>Latest</option>" +
-//                     "<option>Oldest</option>" +
-//                     "<option>Popular</option>" +
-//                     "<option>Relevant</option>" +
-//                 "</select>" +
-//             "</div>" +
-//             "<div class='place-count'>" +
-//                 "<p><%= @places.length %> Places Found.</p>" +
-//             "</div>" +
-//         "</div>" +
-//     "</div>" +
-// "</section>";
