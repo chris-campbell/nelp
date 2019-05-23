d044d1b6ac5e3952b5428d7ac8e8fd67608a1192
Kris Camp <kris.camp@outlook.com>
2019-05-23 00:50:48 +0000

    Refactor toggle Javascript and reduce amount of lines

--- app/assets/javascripts/nav_toggle.js
+++ app/assets/javascripts/nav_toggle.js
@@ -1,42 +1,21 @@
-
-
 $(function () {
     $(document).scroll(function () {
+        const element = $('.buffer');
         var documentCurrentPos = $(this).scrollTop();
-        const element = document.querySelector('.places');
-        const relevanceElement = element.scrollTop = 768;
+        var position = element.position();
+        var elementPos = element.scrollTop = (position.top - 59);
 
-        // Navbar HTML
-        const navbar = "<header>" + 
-                            '<nav class="navbar navbar-light bg-light">' +
-                                '<a class="navbar-brand" href="#">Nelp</a>' + 
-                            "</nav>"+
-                       "</header>";
-                       
-        // Relevance HTML
-        const relevance = "<section class='relevance'>" + "<div class='container'>" +
-        "<div class='row'>" + "<div class='col-md-4'></div>" +
-            "<div class='col-md-4'>" + 
-                '<select class="form-control" id="sel1">' +
-                    "<option>Latest</option>" +
-                    "<option>Oldest</option>" +
-                    "<option>Popular</option>" +
-                    "<option>Relevant</option>" +
-                "</select>" +
-            "</div>" +
-            "<div class='place-count'>" +
-                // "<p><%= @places.length %> Places Found.</p>" +
-            "</div>" +
-            "</div>" +
-            "</div>" +
-        "</section>";
-        
         // Toggle logic for Nav/Relevance
-        if (documentCurrentPos > relevanceElement) {
-            $('.navbar').replaceWith(relevance);
-        } else if (documentCurrentPos < relevanceElement) {
-            $('.relevance').replaceWith(navbar);
+        if (documentCurrentPos > elementPos) {
+            if ($('.relevance').css('display') == 'none') {
+                $('.navbar').toggle();
+                $('.relevance').toggle();
+            }
+        } else if (documentCurrentPos < elementPos) {
+             if ($('.relevance').css('display') == 'block') {
+                $('.navbar').toggle();
+                $('.relevance').toggle();
+            }
         }
-        
     });
 });
