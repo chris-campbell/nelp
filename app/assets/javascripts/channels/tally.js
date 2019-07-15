91132c531c464661546b3c864b0b7293b6b76021
Christopher Campbell <christopher.campbell@outlook.com>
2019-07-15 14:20:23 -0400

    Update style for displaying score percentage
    - Add font awesome percent icon to percent
    - Modify styles to place-item section
    - Add url to place.photos[0].picture (Can't resolve image into URL: undefined method `to_model')
    -

--- app/assets/javascripts/channels/tally.js
+++ app/assets/javascripts/channels/tally.js
@@ -6,7 +6,7 @@ App.tally = App.cable.subscriptions.create('TallyChannel',{
       const placeID = data.place_id;
       let scoreElement = $('.score-value[data-score=' + placeID + ']')
       $(scoreElement[0]).replaceWith("<span class='score-value' data-score='"
-                                + placeID + "'>" + data.content + "</span>");
+                                + placeID + "'>" + data.content + "<i class='fa fa-percent'></i></span>");
     }
   }
 }
